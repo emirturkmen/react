a624642569c6a39264cfc1eafce7307399011e2f
emirturkmen <e.turkmen@etu.edu.tr>
2025-03-23 21:23:23 +0300

    created a nice looking window with a button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-function MyButton() {
-  return <button>I'm a button</button>;
-}
+import React, { useState } from "react";
+import "./App.css";
 
-export default function MyApp() {
+function App() {
+  const [isAnimated, setIsAnimated] = useState(false);
+
+  const handleClick = () => {
+    setIsAnimated(true);
+    setTimeout(() => setIsAnimated(false), 700);
+  };
+
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to React</h1>
+        <p>
+          This is a beautifully styled React application. Edit{" "}
+          <code>src/App.js</code> and save to reload.
+        </p>
+        <p>Start building your amazing application now!</p>
+        <button
+          className={`btn ${isAnimated ? "btn-animated" : ""}`}
+          onClick={handleClick}
+        >
+          Get Started
+        </button>
+      </header>
     </div>
   );
 }
+
+export default App;
